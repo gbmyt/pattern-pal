076b9cb6f3c3add0295e8ccd4b7a49abba5d7ba7
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-09-20 14:34:27 -0400

    Clicking on a pixel will change its color. Allow user to choose color TODO

--- components/Pixel.tsx
+++ components/Pixel.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function GridPixel() {
-  return <div className="border-solid border-2 w-4">p</div>;
+  const [clickState, setClickState] = useState(false);
+  function handleClick(e: React.MouseEvent) {
+    const target = e.target as HTMLInputElement;
+    if (target) {
+      // TODO remove hard-coded pixel fill color
+      setClickState(!clickState);
+      if (!clickState) {
+        target.classList.add("bg-green-700");
+      } else if (clickState) {
+        target.classList.remove("bg-green-700");
+      }
+    }
+  }
+  return (
+    <div onClick={handleClick} className="border-solid border-2 w-full">
+      p
+    </div>
+  );
 }
 
 export default GridPixel;
