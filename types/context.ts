c4458b09c6ecdd0abe3764b1634f8f33a95bb043
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-10-15 17:01:13 -0400

    Added maxGridSize value to GridContext

--- types/context.ts
+++ types/context.ts
@@ -2,6 +2,7 @@ import { SetStateAction } from "react";
 
 export type GridContextType = {
   defaultFillColor: string;
+  maxGridWidth: number;
   pixelFillColor: string;
   setPixelFillColor: React.Dispatch<SetStateAction<string>>;
 };
