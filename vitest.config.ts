a7b80595bf523c9d749162bc85acdc96a0aaa785
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-10-23 16:20:00 -0400

    Add vite-tsconfig-paths to vitest config/test setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    include: ["./**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    environment: "jsdom",
-    setupFiles: "./tests/setup.ts",
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "html", "clover", "json"],
-      reportsDirectory: "./tests/coverage",
+    plugins: [tsconfigPaths(), react()],
+    test: {
+        globals: true,
+        include: ["./**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        environment: "jsdom",
+        setupFiles: "./tests/setup.ts",
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "html", "clover", "json"],
+            reportsDirectory: "./tests/coverage",
+        },
     },
-  },
-});
+})
