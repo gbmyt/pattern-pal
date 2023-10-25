4f80ac0b97b78a3d1c4ac0037abcd8f769b59b5c
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-10-25 16:34:06 -0400

    Added Prisma Client mock for testing. Added to lib/__mocks__ folder instead of root __mocks__ so that its nested next to the file that it will be mocking (lib/db.ts, which is the 'real' database client)

---
+++ lib/__mocks__/db.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client"
+import { beforeEach } from "vitest"
+import { mockDeep, mockReset } from "vitest-mock-extended"
+
+beforeEach(() => {
+    mockReset(db)
+})
+
+const db = mockDeep<PrismaClient>()
+export default db
