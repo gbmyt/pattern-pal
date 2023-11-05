c94aad929d790e811ad030e546c9ac5cfa0eac58
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-11-05 15:16:37 -0500

    Fix Typescript build errors caused by wrong pixels array type in usePixelFillState hook

--- hooks/usePixelFillState.tsx
+++ hooks/usePixelFillState.tsx
@@ -1,27 +1,19 @@
 "use client"
 import { useGridContext } from "@/context/GridContext"
-import { Pattern } from "@prisma/client"
-import { SetStateAction, useState } from "react"
+import { useState } from "react"
 
 export function usePixelIsFilled() {
     const [pixelIsFilled, setPixelIsFilled] = useState(false)
     const { pixelFillColor, setPattern } = useGridContext()
 
-    function fillGridPixel(
-        pixel: HTMLDivElement,
-        pixels: string | undefined = undefined,
-        position: string | undefined = undefined
+    function updatePixelFillStateInGrid(
+        type: string,
+        pixels: string[][],
+        position: string
     ) {
-        pixel.style.backgroundColor = ""
-        pixel.style.backgroundColor = pixelFillColor
-
-        if (pixels && position) {
-            let x = parseInt(position.split(",")[0])
-            let y = parseInt(position.split(",")[1])
-
-            var gridPosition = pixels[x][y]
-            pixels[x][y] = !gridPosition && pixelFillColor
-        }
+        let x = parseInt(position.split(",")[0])
+        let y = parseInt(position.split(",")[1])
+        pixels[x][y] = type === "fill" ? pixelFillColor : ""
 
         try {
             setPattern((prevState) => ({
@@ -29,32 +21,32 @@ export function usePixelIsFilled() {
                 pixels: JSON.stringify(pixels),
             }))
         } catch (e) {
-            console.log("Couldn't fill grid pixel")
+            console.log("Couldn't update pixel grid")
+            return e
+        }
+    }
+
+    function fillGridPixel(
+        pixel: HTMLDivElement,
+        pixels: string[][] | undefined = undefined,
+        position: string | undefined = undefined
+    ) {
+        pixel.style.backgroundColor = ""
+        pixel.style.backgroundColor = pixelFillColor
+        if (pixels && position) {
+            updatePixelFillStateInGrid("fill", pixels, position)
         }
     }
 
     function removePixelFill(
         pixel: HTMLDivElement,
-        pixels: string | undefined = undefined,
+        pixels: string[][] | undefined = undefined,
         position: string | undefined = undefined
     ) {
         pixel.style.backgroundColor = ""
 
         if (pixels && position) {
-            let x = parseInt(position.split(",")[0])
-            let y = parseInt(position.split(",")[1])
-
-            var gridPosition = pixels[x][y]
-            pixels[x][y] = gridPosition && null
-        }
-
-        try {
-            setPattern((prevState) => ({
-                ...prevState,
-                pixels: JSON.stringify(pixels),
-            }))
-        } catch (e) {
-            console.log("Couldn't remove pixel fill")
+            updatePixelFillStateInGrid("clear", pixels, position)
         }
     }
 
