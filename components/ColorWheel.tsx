3dba5432347fda1567121b0748a21bb87369e335
Gabriela <48141726+gbmyt@users.noreply.github.com>
2024-02-13 14:54:35 -0500

    Replaced the color input field with a Color Wheel, added a 'Reset Color to Default' option, and extracted all the Editor menu controls to their own component

--- components/ColorWheel.tsx
+++ components/ColorWheel.tsx
@@ -1,16 +1,23 @@
+import { useGridContext } from "@/context/GridContext"
 import Sketch from "@uiw/react-color-wheel"
 // import { Alpha, Hue, ShadeSlider, Saturation, Interactive, hsvaToHslaString } from '@uiw/react-color';
 // import { EditableInput, EditableInputRGBA, EditableInputHSLA } from '@uiw/react-color';
-import { useState } from "react"
+import { SetStateAction, useContext, useState } from "react"
 
-function ColorWheel() {
-    const [hex, setHex] = useState("#fff")
+function ColorWheel({ disabled }: { disabled: boolean }) {
+    const { pixelFillColor, setPixelFillColor } = useGridContext()
+    const [hex, setHex] = useState(pixelFillColor)
     return (
         <Sketch
-            style={{ marginLeft: 20 }}
+            style={
+                disabled
+                    ? { pointerEvents: "none", opacity: "0.4" }
+                    : { marginLeft: 20 }
+            }
             color={hex}
             onChange={(color) => {
                 setHex(color.hex)
+                setPixelFillColor(color.hex)
             }}
         />
     )
