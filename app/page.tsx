e0a79ebe100cf3c4bfdef3487d7506b24793dbd7
Gabriela <48141726+gbmyt@users.noreply.github.com>
2024-02-20 14:06:33 -0500

    - Added a new folder under '*/public/symbols' for housing SVG and PNG images of standard knit/crochet stitches.
    - Added a new route for displaying the stitch library. Currently just imports the SVG images and renders them in an Next Image component.
    
    TODO: Figure out how to dynamically add SVG exports to the `symbols/index.tsx` file. Currently they must be added to the export list manually in order to render in browser.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+// import Image from "next/image"
 import Main from "./Main"
 
 export default function Home() {
---
+++ public/symbols/index.tsx
@@ -0,0 +1,6 @@
+import * as KnitSVG from "./svg/knit.svg"
+import * as PurlSVG from "./svg/purl.svg"
+import * as SlipSVG from "./svg/slip.svg"
+
+const images = [{ ...KnitSVG }, { ...PurlSVG }, { ...SlipSVG }]
+export default images
