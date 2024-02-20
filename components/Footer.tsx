6394cab86672f7c10a3ab7efca5b4e212bb1fbb9
Gabriela <48141726+gbmyt@users.noreply.github.com>
2024-02-20 14:45:13 -0500

    Added links to footer component, some of them are dead and need to be implemented. Moved the FAQ link from the Card Carousel on the home page to the footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,40 +1,95 @@
-import Link from "next/link";
-import siteMetadata from "@/data/siteMetadata";
-import SocialIcon from "@/components/social-icons";
+import Link from "next/link"
+import siteMetadata from "@/data/siteMetadata"
+import SocialIcon from "@/components/social-icons"
 
-// To-Do
-// const FOOTER_LINKS = [
-//   "About",
-//   "Terms of Service",
-//   "Help improve this page",
-// ];
+// TODO: Instead of rendering 2 separate lists, adjust styles to create
+// new columns on the fly/as-needed/dynamically
+const FOOTER_LINKS = {
+    main: [
+        { linkText: "About", href: "/about" },
+        { linkText: "FAQ", href: "/faq" },
+        { linkText: "Contact", href: "/contact" },
+        { linkText: "Careers", href: "/careers" },
+    ],
+    secondary: [
+        { linkText: "Contributors", href: "/contributors" },
+        { linkText: "Terms of Service", href: "/terms" },
+        { linkText: "Privacy Policy", href: "/privacy-policy" },
+        { linkText: "Legal", href: "/legal" },
 
-function Footer() {
-  return (
-    <footer>
-      <div className="mt-16 flex flex-col items-center">
-        <div className="mb-3 flex space-x-4">
-          <SocialIcon
-            kind="mail"
-            href={`mailto:${siteMetadata.email}`}
-            size={6}
-          />
-          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-        </div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/gbmyt"> Gabriela Taylor</Link>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href={siteMetadata.siteRepo}>{siteMetadata.title}</Link>
-        </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          The content of this site is licensed under the MIT License.
-        </div>
-      </div>
-    </footer>
-  );
+        // { linkText: "Help improve this page", href: "/feedback" },
+    ],
 }
 
-export default Footer;
+function Footer() {
+    return (
+        <footer>
+            <hr />
+            <div className="flex flex-row m-8" id="footer-links">
+                <div className="ml-32 my-8">
+                    {FOOTER_LINKS.main.map((link, index) => (
+                        <a
+                            className="flex text-xs my-2 text-gray-300 hover:text-purple-300"
+                            href={link.href}
+                            key={index}
+                        >
+                            {link.linkText}
+                        </a>
+                    ))}
+                </div>
+                <div className="mx-16 my-8">
+                    {FOOTER_LINKS.secondary.map((link, index) => (
+                        <a
+                            className="flex text-xs my-2 text-gray-300
+                            hover:text-purple-300"
+                            href={link.href}
+                            key={index}
+                        >
+                            {link.linkText}
+                        </a>
+                    ))}
+                </div>
+            </div>
+
+            <div className="mt-4 flex flex-col items-center">
+                {/* Social Icons/Links */}
+                <div className="mb-3 flex space-x-4">
+                    <SocialIcon
+                        kind="mail"
+                        href={`mailto:${siteMetadata.email}`}
+                        size={6}
+                    />
+                    <SocialIcon
+                        kind="github"
+                        href={siteMetadata.github}
+                        size={6}
+                    />
+                    <SocialIcon
+                        kind="linkedin"
+                        href={siteMetadata.linkedin}
+                        size={6}
+                    />
+                </div>
+
+                {/* Site References/Contributor Links */}
+                <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+                    <Link href="https://github.com/gbmyt">
+                        {" "}
+                        Gabriela Taylor
+                    </Link>
+                    <div>{` • `}</div>
+                    <div>{`© ${new Date().getFullYear()}`}</div>
+                    <div>{` • `}</div>
+                    <Link href={siteMetadata.siteRepo}>
+                        {siteMetadata.title}
+                    </Link>
+                </div>
+                <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+                    The content of this site is licensed under the MIT License.
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
