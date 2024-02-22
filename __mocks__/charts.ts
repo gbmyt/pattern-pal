222fca2abe044072c776773a6d9df2180ef3ebc1
Gabriela <48141726+gbmyt@users.noreply.github.com>
2024-02-22 15:39:19 -0500

    Renamed pattern mocks to 'charts'

--- __mocks__/charts.ts
+++ __mocks__/charts.ts
@@ -1,4 +1,6 @@
-const mockPatterns = [
+import { ChartTestType } from "@/types/chart"
+
+const mockCharts: ChartTestType[] = [
     {
         id: "1",
         createdAt: "2023-10-25T19:46:37.781Z" as unknown as Date,
@@ -6,18 +8,25 @@ const mockPatterns = [
         gridHeight: 3,
         gridWidth: 3,
         pixels: '[["#0000FF",null,"#0000FF"],[null,null,null],[null,"#0000FF",null]]',
+        userId: "1",
     },
     {
+        id: "2",
+        createdAt: "2023-10-25T19:46:37.781Z" as unknown as Date,
         title: "Anotha",
         gridHeight: 5,
         gridWidth: 5,
         pixels: '[["#0000FF",null,"#0000FF", null,"#0000FF"],[null,null, null,null,null],[null,"#0000FF",null,"#0000FF",null]],[null,null, null,null,null],[null,"#0000FF",null,"#0000FF",null]',
+        userId: "1",
     },
     {
+        id: "3",
+        createdAt: "2023-10-25T19:46:37.781Z" as unknown as Date,
         title: "Empty One",
         gridHeight: 3,
         gridWidth: 3,
         pixels: '[[null,null,null],[null,null,null],[null,"#0000FF",null]]',
+        userId: "1",
     },
 ]
-export default mockPatterns
+export default mockCharts
