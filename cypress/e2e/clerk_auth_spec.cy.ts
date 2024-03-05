6d7b9cc844da12ffb2e03eb7243cd0e58070489a
Gabriela <48141726+gbmyt@users.noreply.github.com>
2024-03-05 12:00:11 -0500

    Clerk Auth spec now tests both the Sign Up and Sign In user flows. It visits the url programmatically instead of attempting to navigate from the home page to sign up/sign in and currently does not fully submit the Sign Up form from the UI, as there is bot detection in place that causes an error when signing up. Custom Cypress command TODO

--- cypress/e2e/clerk_auth_spec.cy.ts
+++ cypress/e2e/clerk_auth_spec.cy.ts
@@ -4,12 +4,10 @@ describe("Auth", () => {
     describe("Sign Up", function () {
         // Before each test click Sign up button so that we get the Clerk generated url string with the correct redirect path
         beforeEach(() => {
-            cy.visit("/", { failOnStatusCode: false })
-            cy.get("button").contains("Sign up").should("be.visible").click()
-        })
-
-        it("Navigates to the Sign Up page", () => {
-            cy.url().should("include", "/sign-up")
+            cy.visit(
+                "/sign-up?after_sign_in_url=%2Feditor&after_sign_up_url=%2Fnew-user&redirect_url=%2F",
+                { failOnStatusCode: false }
+            )
         })
 
         it("Renders Sign Up page header", () => {
@@ -36,8 +34,24 @@ describe("Auth", () => {
                 .should("match", /Sign In/i)
         })
 
-        // This fails because Clerk detects 'bot traffic'. Need to do this programmatically
-        it.skip("Allows the user to create an account", () => {
+        it("errors on a blank email input", function () {
+            cy.get("button:contains('Continue')").click()
+            cy.url().should("include", "/sign-up")
+
+            // Cannot detect Chrome Tooltip, can't even click on it to inspect in browser
+            // cy.contains(/Please fill out this field./i).should("exist")
+        })
+
+        it("errors on a blank password input", function () {
+            cy.get("input#emailAddress-field")
+                .click()
+                .type(Cypress.env(`test_email`))
+
+            cy.get("button:contains('Continue')").click()
+            cy.url().should("include", "/sign-up")
+        })
+
+        it("Allows the user to create an account", () => {
             // Create your account
             cy.get("input#emailAddress-field")
                 .click()
@@ -45,59 +59,74 @@ describe("Auth", () => {
             cy.get("input#password-field")
                 .click()
                 .type(Cypress.env(`test_password`))
+
             // Press the button to create an account
             cy.get("button:contains('Continue')").click()
-        })
+            cy.contains(
+                "Your password meets all the necessary requirements."
+            ).should("be.visible")
 
-        it.skip("Redirects to the ChartEditor after sign up", function () {
             // Assert we were redirected to the chart editor (or dashboard? TBD)
-            cy.url().should("include", "/editor")
-            cy.get("user-button").should("exist").and("should:be.visible")
+            // This fails because Clerk detects 'bot traffic' when attempting to sign up. Debug TODO
+            // cy.url().should("include", "/editor")
+            // cy.get("user-button").should("exist").and("should:be.visible")
         })
 
+        // it('email/username has already been taken', function() {});
         // it("allows third-party sign-up via Gmail/Facebook/Apple", () => {})
     })
 
-    // describe.skip("Sign In", () => {
-    //     before(() => {
-    //         // sign in
-    //         // cy.signIn()
-    //     })
+    describe("Sign In", () => {
+        beforeEach(() => {
+            cy.visit(
+                "/sign-in?after_sign_in_url=%2Feditor&after_sign_up_url=%2Fnew-user&redirect_url=%2F",
+                { failOnStatusCode: false }
+            )
+        })
 
-    //     it("Navigates to the Sign In page", () => {
-    //         cy.url().should("include", "/sign-in")
-    //     })
+        it("Navigates to the Sign In page", () => {
+            cy.url().should("include", "/sign-in")
+        })
 
-    //     it("Renders Sign In page header", () => {
-    //         cy.get("h1")
-    //             .invoke("text")
-    //             .should("match", /Sign in/i)
-    //     })
+        it("Renders Sign In page header", () => {
+            cy.get("h1")
+                .invoke("text")
+                .should("match", /Sign in/i)
+        })
 
-    //     it("Renders email input", () => {
-    //         cy.get("label")
-    //             .invoke("text")
-    //             .should("match", /Email Address/i)
-    //     })
+        it("Renders email input", () => {
+            cy.get("label")
+                .invoke("text")
+                .should("match", /Email Address/i)
+        })
 
-    //     it("Renders password input", () => {
-    //         cy.get("label")
-    //             .invoke("text")
-    //             .should("match", /Password/i)
-    //     })
+        it("Renders password input", () => {
+            cy.get("label")
+                .invoke("text")
+                .should("match", /Password/i)
+        })
 
-    //     it("Prompts the user to Sign up if they don't have an account", () => {
-    //         cy.get("a")
-    //             .invoke("text")
-    //             .should("match", /Sign up/i)
-    //     })
+        it("Prompts the user to Sign up if they don't have an account", () => {
+            cy.get("a")
+                .invoke("text")
+                .should("match", /Sign up/i)
+        })
 
-    // })
+        it("errors on a blank email input", function () {
+            cy.get("button:contains('Continue')").click()
+            cy.url().should("include", "/sign-in")
 
-    // describe.skip("Sign Out", function () {
-    //     it.skip("Protects the correct routes when logged out", function () {})
-    //     it.skip("Redirects to the (editor or sign in page) after logging out", function () {
-    //         cy.url().should("include", "/editor")
-    //     })
-    // })
+            // Cannot detect Chrome Tooltip, can't even click on it to inspect in browser
+            // cy.contains(/Please fill out this field./i).should("exist")
+        })
+
+        it("errors on a blank password input", function () {
+            cy.get("input#identifier-field")
+                .click()
+                .type(Cypress.env(`test_email`))
+
+            cy.get("button:contains('Continue')").click()
+            cy.url().should("include", "/sign-in")
+        })
+    })
 })
