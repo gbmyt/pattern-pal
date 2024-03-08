859f2604ce99fa90a18e425d5f7fb78125550cc7
Gabriela <48141726+gbmyt@users.noreply.github.com>
2024-03-08 13:11:41 -0500

    Extracted global Tailwind style classes to their own file and removed hardcoded values from Modal.tsx

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { SetStateAction } from "react"
 import Button from "./Button"
+import { transitionStyles, withDropShadowBorder } from "@/data/styles"
 
 const Modal = ({
     isOpen,
@@ -14,12 +15,6 @@ const Modal = ({
     // Default Styles
     var hidden = "opacity-0 h-0"
     var visible = "h-fit min-h-1/4 opactity-100"
-
-    // Extra Styles
-    var withDropShadowBorder = "border-2 rounded-xl drop-shadow-xl"
-    var transitionStyles = "transition-all ease-in-out delay-100 duration-250"
-
-    var withBorder = "border-2 border-solid border-red-200"
 
     var overlayWrapper =
         "fixed z-50 h-fit min-h-3/4 w-full max-w-xl m-auto inset-x-0 inset-y-0 p-4 rounded-sm overflow-y-scroll bg-white"
