8a7fcff1d966e2cee3ec26ad1efa33159a409180
Gabriela <48141726+gbmyt@users.noreply.github.com>
2024-03-08 14:50:22 -0500

    Map over an array of sidebar links, instead of duplicating anchor link code

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -3,6 +3,13 @@ import { transitionStyles } from "@/data/styles"
 const sideBarLinkStyles =
     "group rounded-lg border border-transparent first-letter:transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 text-sm p-4"
 
+const sideBarLinks = [
+    { href: "/editor", linkText: "Chart Editor" },
+    { href: "/symbols", linkText: "Symbols" },
+    { href: "/templates", linkText: "Templates" },
+    { href: "/community", linkText: "Community" },
+]
+
 const SideBar = async () => {
     return (
         <div
@@ -11,23 +18,16 @@ const SideBar = async () => {
             <div
                 className={`flex flex-col h-fit min-w-max m-0 py-4 pl-4 pr-8 bg-opacity-100 bg-slate-800 rounded-md`}
             >
-                <a href="/editor">
-                    <h2 className={sideBarLinkStyles}>Chart Editor</h2>
-                </a>
-
-                <a href="/symbols">
-                    <h2 className={sideBarLinkStyles}>Chart Symbols</h2>
-                </a>
-                <a href="/dashboard">
-                    <h2 className={sideBarLinkStyles}>Dashboard </h2>
-                </a>
-                <a href="/templates">
-                    <h2 className={sideBarLinkStyles}>Templates </h2>
-                </a>
-
-                <a href="/community">
-                    <h2 className={sideBarLinkStyles}>Community </h2>
-                </a>
+                {sideBarLinks &&
+                    sideBarLinks.map((link, index) => (
+                        <a
+                            className={sideBarLinkStyles}
+                            key={index}
+                            href={link.href}
+                        >
+                            <h2>{link.linkText}</h2>
+                        </a>
+                    ))}
             </div>
         </div>
     )
