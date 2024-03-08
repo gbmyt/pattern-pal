15224b884be7643caa51fa37283852a36374611b
Gabriela <48141726+gbmyt@users.noreply.github.com>
2024-03-08 14:41:29 -0500

    Stub out SearchBar component, currently using it to confirm ability to set the editorFillState -- search component only becomes visible if the mode is set to 'Symbol'

--- components/ChartEditor.tsx
+++ components/ChartEditor.tsx
@@ -1,8 +1,11 @@
 import { auth } from "@clerk/nextjs"
+import { Suspense } from "react"
+
+// Components
 import EditorForm from "./EditorForm"
 import Grid from "./Grid"
 import RecentChartsList from "./RecentChartsList"
-import { Suspense } from "react"
+import SearchBar from "./SearchBar"
 
 async function ChartEditor() {
     const { userId } = await auth()
@@ -10,9 +13,12 @@ async function ChartEditor() {
     return (
         <>
             <EditorForm authorized={userId ? true : false} />
+
             <Suspense fallback={<h2>Loading...</h2>}>
+                <SearchBar />
                 <Grid />
             </Suspense>
+
             <Suspense fallback={<h2>Loading...</h2>}>
                 <RecentChartsList />
             </Suspense>
