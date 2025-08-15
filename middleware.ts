6b50aa97e07e23b268e19034ef41cbc2b37f841b
Gabriela <48141726+gbmyt@users.noreply.github.com>
2025-08-15 11:30:50 -0400

    Ugrade Next JS/Clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,57 @@
-import { authMiddleware } from "@clerk/nextjs"
+import { authMiddleware } from '@clerk/nextjs'; 
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+const publicRoutes = [
+    // main
+    "/",
+    "/login",
+    "/about",
+    "/dashboard",
+
+    // account
+    "/account",
+    "/inbox",
+    "/stash",
+    "/queue",
+    "/library",
+    "/favorites",
+    // "/projects",
+    "/projects/:slug",
+    "/settings",
+    "/cart",
+
+    // studio
+    "/studio",
+    "/templates",
+    "/editor",
+    "/patterns",
+    "/patterns/:slug",
+
+    // seller
+    "/sell",
+    "/shop",
+    "/shop/:slug",
+    "/shop/manage",
+
+    // community
+    "/community",
+    "/contributors",
+
+    // miscellaneous
+    "/symbols",
+    "/faq",
+
+    // API
+    "/about",
+    "/api/patterns",
+    "/api/auth",
+    "/api/callback",
+    "/callback",
+];
+
 export default authMiddleware({
-    publicRoutes: [
-        "/",
-        "/about",
-        "/dashboard",
-        "/symbols",
-        "/editor",
-        "/studio",
-        "/sell",
-        "/community",
-        "/faq",
-        "/contributors",
-        "/templates",
-    ],
+    publicRoutes,
 })
 
 export const config = {
-    runtime: "nodejs",
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+  matcher: ['/((?!_next/image|_next/static|favicon.ico).*)'],
+};
